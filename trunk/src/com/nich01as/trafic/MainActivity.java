f6fdce274bbd32f9e407b64642d3b0f9a9c7dbe6
nicholas.zhaoyu@gmail.com <nicholas.zhaoyu@gmail.com@4615e9e2-88a7-9077-cff8-da09c3610a09>
2011-11-23 03:44:27 +0000

    ADD: create the project for trafic.

---
+++ trunk/src/com/nich01as/trafic/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nich01as.trafic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
